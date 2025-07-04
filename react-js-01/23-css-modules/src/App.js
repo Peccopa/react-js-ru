932afbfd4038db9b5fe7e074c971572636252b1b
Andrey Zharkikh <orbital.logistics@yandex.ru>
2025-07-04 16:42:24 +0300

    feat: add react-01-23

--- react-js-01/23-css-modules/src/App.js
+++ react-js-01/23-css-modules/src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <Info />
+      <div className="info">
+        <h1>App component heading</h1>
+        <button className="my-button">App component button</button>
+        <h2>Header H2 App</h2>
+      </div>
     </div>
   );
 }
--- react-js-01/23-css-modules/src/components/info.js
+++ react-js-01/23-css-modules/src/components/info.js
@@ -1,9 +1,16 @@
+// import './info.css';
+import styles from './info.module.css';
+
+console.log(styles);
+
 function Info() {
   return (
-    <>
+    <div className={styles.info}>
       <h1>Hello from the info component</h1>
-      <button>Click me!</button>
-    </>
+      <h2>Header H2 Info</h2>
+      <button className="my-button">Click me in the info component</button>
+      <button className={styles.myOtherButton}>Button with local CSS</button>
+    </div>
   );
 }
 
